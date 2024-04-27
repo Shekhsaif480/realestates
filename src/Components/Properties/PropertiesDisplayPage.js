f8df2ef1be234a1d43287615205a45749b7948de
Saif Shaikh <shekhsaif480@gmail.com>
2024-04-27 11:18:37 +0530

    all

--- src/Components/Properties/PropertiesDisplayPage.js
+++ src/Components/Properties/PropertiesDisplayPage.js
@@ -156,7 +156,7 @@ const PropertiesDisplayPage = () => {
       
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {filteredProperties.map(property => (
-          <PropertyCard key={property.id} property={property} />
+          <PropertyCard key={property.id} property={property}/>
         ))}
       </div>
     </div>
--- src/Components/Properties/PropertyDetail.js
+++ src/Components/Properties/PropertyDetail.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 import { doc, getDoc } from 'firebase/firestore';
 import { txtDB } from '../../firebase';
 
 const PropertyDetail = () => {
   const [property, setProperty] = useState(null);
-  const { id } = useParams(); // Get the ID from URL parameters
+  const id = '04bJQLDEa9QYDJCFlJhI'; // Placeholder ID, replace with actual ID or use useParams()
 
   useEffect(() => {
     const fetchProperty = async () => {
@@ -35,14 +34,15 @@ const PropertyDetail = () => {
   return (
     <div className="max-w-4xl mx-auto px-4 py-8">
       <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-        <img src={imgUrl} alt={name} className="w-full h-64 object-cover object-center" />
+        <img src={imgUrl} alt={name} className="w-full h-64 object-cover object-center rounded-t-lg" />
         <div className="p-6">
           <h2 className="text-3xl font-semibold mb-4">{name}</h2>
           <p className="text-gray-600 mb-2">{address}</p>
-          <p className="text-gray-700 font-semibold mb-2">${price}</p>
+          <p className="text-blue-900 font-bold mb-2">₹{price}</p>
           <p className="text-gray-700 mb-2">Area: {area_in_square_fts} sq.ft</p>
           <p className="text-gray-700 mb-2">Bedrooms: {no_of_bedrooms}</p>
           <p className="text-gray-700 mb-4">Washrooms: {no_of_washrooms}</p>
+          <p className="text-gray-700 mb-4">Contact: +91 8422033730 </p>
           <div className="mb-4">
             <h3 className="text-xl font-semibold mb-2">Amenities:</h3>
             <ul className="list-disc list-inside">
